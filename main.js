00cd597e4d56b5cc027e88b2fa7fd7b8b0670612
Blondy Martinez <47917655+BlondyMartinez@users.noreply.github.com>
2024-02-08 22:20:39 +0000

    bookmark + minor fixes

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-function like(card) {
+function like(card) { 
     var icon = card.querySelector('.icon');
     var likesCount = card.querySelector('.likes-count');
+
     var currentLikesCount = parseInt(likesCount.textContent);
+    
+    changeIcon(icon);
 
     if(icon.classList.contains('fa-solid')) {
-        icon.classList.remove('fa-solid');
-        icon.style.color = 'black';
-        likesCount.textContent = currentLikesCount - 1;
-    } else {
-        icon.classList.add('fa-solid');
         icon.style.color = 'red';
         likesCount.textContent = currentLikesCount + 1;
+    } else {
+        icon.style.color = 'black';
+        likesCount.textContent = currentLikesCount - 1;
     }
+}
+
+function changeIcon(icon){
+    if(icon.classList.contains('fa-solid')) icon.classList.remove('fa-solid');
+    else icon.classList.add('fa-solid');
 }
